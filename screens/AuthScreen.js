51676d1859a10e4a54f6fbcd7c3e321d80d4d3c6
takanoria0612 <takanorihiraki06@gmail.com>
2023-06-06 19:40:34 +0900

    commit

---
+++ screens/AuthScreen.js
@@ -0,0 +1,46 @@
+import { Feather, FontAwesome } from '@expo/vector-icons';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Input from '../components/Input';
+import PageContainer from '../components/PageContainer';
+import SubmitButton from '../components/SubmitButton';
+
+const AuthScreen = props => {
+    
+    return <SafeAreaView style={{ flex: 1}}>
+        <PageContainer>
+            <Input
+                label="First name"
+                icon="user-o"
+                iconPack={FontAwesome} />
+
+            <Input
+                label="Last name"
+                icon="user-o"
+                iconPack={FontAwesome} />
+
+            <Input
+                label="Email"
+                icon="mail"
+                iconPack={Feather} />
+
+            <Input
+                label="Password"
+                icon="lock"
+                iconPack={Feather} />
+            
+            <SubmitButton
+                title="Sign up"
+                onPress={() => console.log("Button pressed")}
+                style={{ marginTop: 20 }}/>
+        </PageContainer>
+    </SafeAreaView>
+};
+
+const styles = StyleSheet.create({
+    
+})
+
+export default AuthScreen;
